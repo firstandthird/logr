6832ab25157f228498ed52588ad0ca496a676284
Greg Allen <me@jga.me>
2012-01-17 20:28:15 -0800

    renamed core to dispatcher

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var Core = require('./lib/core');
+var Dispatcher = require('./lib/dispatcher');
 var Logger = require('./lib/logger');
 
 
-var core;
+var dispatcher;
 module.exports = function(name, section, options) {
   if (typeof section === 'object') {
     options = section;
     section = '';
   }
-  if (!core)
-    core = new Core(options);
-  var logger = new Logger(core, name, section, options);
-  //core.addLogger(logger);
+  if (!dispatcher)
+    dispatcher = new Dispatcher(options);
+  var logger = new Logger(dispatcher, name, section, options);
+  dispatcher.useLogger(logger);
   return logger;
 };
