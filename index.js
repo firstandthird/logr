8909d8ec305675d3fb0960875f2d0f9471230760
Greg Allen <me@jga.me>
2016-03-01 11:03:53 -0800

    moved renderers to separate file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const aug = require('aug');
-const stringify = require('json-stringify-safe');
 const defaults = {
   type: 'console',
   timestamp: true,
@@ -18,30 +17,8 @@ class Logger {
     this.config = aug({}, defaults, options);
 
     this.renderers = {
-      console: (tags, message) => {
-        const ts = (this.config.timestamp) ? `${new Date().toTimeString()}: ` : '';
-        if (typeof message === 'object') {
-          message = stringify(message);
-        }
-        const out = `${ts}[${tags.join(',')}] ${message}`;
-        return out;
-      },
-      json: (tags, message) => {
-        if (this.config.jsonOptions.tagsObject) {
-          const tagsObj = {};
-          tags.forEach((tag) => {
-            tagsObj[tag] = true;
-          });
-          tags = tagsObj;
-        }
-        let out = {
-          timestamp: new Date(),
-          tags,
-          message
-        };
-        out = aug(out, this.config.jsonOptions.additional);
-        return stringify(out);
-      }
+      console: require('./lib/console'),
+      json: require('./lib/json')
     };
 
     if (this.config.type && !this.renderers[this.config.type]) {
@@ -65,7 +42,7 @@ class Logger {
       return;
     }
 
-    const out = this.renderers[this.config.type](tags, message);
+    const out = this.renderers[this.config.type](this.config, tags, message);
     /*eslint-disable no-console*/
     console.log(out);
     /*eslint-enable no-console*/
